daafbae10d8fe7edbf4bb0a4f2fae8bd7056ef76
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-01-13 07:05:55 -0800

    new tutorial

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,27 +7,44 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Button("hello world", systemImage: "trash"){
+//                print(type(of: self.body))
+//            }
+////            Image(systemName: "figure.walk")
+////                .imageScale(.large)
+////                .foregroundStyle(.black)
+////            Text("Hello, world!")
+//                
+////                .background(.regularMaterial)
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .padding()
+//        .background(LinearGradient(colors: [.cyan, .purple], startPoint: .top, endPoint: .bottom))
+////        .opacity(0.5)
+//        .backgroundStyle(.thinMaterial)
+//        
+//    }
+//}
+//
+
+
+
 struct ContentView: View {
+    @State private var useRedText = false
+    
     var body: some View {
-        VStack {
-            Button("hello world", systemImage: "trash"){
-                print(type(of: self.body))
-            }
-//            Image(systemName: "figure.walk")
-//                .imageScale(.large)
-//                .foregroundStyle(.black)
-//            Text("Hello, world!")
-                
-//                .background(.regularMaterial)
+        Button("Use red") {
+            useRedText.toggle()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding()
-        .background(LinearGradient(colors: [.cyan, .purple], startPoint: .top, endPoint: .bottom))
-//        .opacity(0.5)
-        .backgroundStyle(.thinMaterial)
-        
+        .frame(maxWidth: 200, maxHeight: 200)
+        .background(.black)
+        .foregroundStyle(useRedText ? .red : .blue)
     }
 }
+
 
 #Preview {
     ContentView()
